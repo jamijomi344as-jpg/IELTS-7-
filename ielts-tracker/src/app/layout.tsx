e7ed3baee090ada5a3cfe21f96a7bfe54c7fce6b
jamijomi344as-jpg <jamijomi344as@gmail.com>
2026-06-06 21:31:35 +0500

    Update layout.tsx

--- ielts-tracker/src/app/layout.tsx
+++ ielts-tracker/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist } from 'next/font/google';
 import './globals.css';
-import Navbar from '@/components/Navbar';
+import { Navbar } from '@/components/Navbar';
 import TeacherLogin from '@/components/TeacherLogin';
 
 const geist = Geist({ subsets: ['latin'], variable: '--font-geist-sans' });
